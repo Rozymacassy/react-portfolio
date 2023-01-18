8d6ae64c0eec8fffbc5e680e566b8112244bc024
rosemary <mariposamacassy@gmail.com>
2023-01-18 14:05:19 +0100

    modified contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,25 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <h4>Email</h4>
+            <h5>mariposamacassy@gmail.com</h5>
+            <a href="mailto:mariposamacassy@gmail.com"></a>
+
+          </article>
+        </div>
+
+        {/* END OF CONTACT OPTIONS */}
+
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
