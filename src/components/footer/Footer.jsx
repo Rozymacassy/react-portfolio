a9d615a967fcb715d7f267a8341a0d9774b7cba7
rosemary <mariposamacassy@gmail.com>
2023-01-18 17:24:02 +0100

    created the footer markup and styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebook} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {IoLogoTwitter} from 'react-icons/io'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Rozymacassy</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a> </li>
+        <li><a href="#about">About</a> </li>
+        <li><a href="#experience">Experience</a> </li>
+        <li><a href="#services">Services</a> </li>
+        <li><a href="#portfolio">Portfolio</a> </li>
+        <li><a href="#testimonials">Testimonials</a> </li>
+        <li><a href="#contact">Contact</a> </li>
+
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"> <FaFacebook/> </a>
+        <a href="https://twitter.com"> <FaInstagram/> </a>
+        <a href="https://facebook.com"> <IoLogoTwitter/> </a>
+
+      </div>
+
+      <div className="footer__copyright">
+        <small>
+          &copy;2023 | All Rights Reserved | Design by Chinenye Rosemary
+        </small>
+      </div>
+    </footer>
   )
 }
 
